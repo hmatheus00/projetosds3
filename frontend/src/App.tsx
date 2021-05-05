5ca1bc58ef9a891ff38f4c1143635527dc8e3ecd
hmatheus00 <hmatheus00@outlook.com>
2021-05-05 15:57:12 -0300

    Gráficos estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import Barchart from 'components/Barchart';
 import Datatable from 'components/DataTable';
+import Donutcharts from 'components/Donutchart';
 import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
 
@@ -7,6 +9,20 @@ function App() {
       <>
       <Navbar />
       <div className="container">
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Porcentagem de sucesso</h5>
+            <Barchart />
+          </div>
+          <div className="col-sm-6">
+            <h5>Listagem de vendas</h5>
+            <Donutcharts />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Total de vendas</h2>
+        </div>
         <Datatable />
       </div>
       <Footer />
