a502cb5b7642070484321112cead23b6cc3340af
hmatheus00 <hmatheus00@outlook.com>
2021-05-07 18:56:44 -0300

    Add rotas

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,19 @@
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard" exact>
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
