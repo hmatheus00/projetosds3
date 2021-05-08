fb582e98e16f43d8146e6d2664c92c0a37ca6e06
hmatheus00 <hmatheus00@outlook.com>
2021-05-08 18:29:17 -0300

    gráficos com dados reais

--- frontend/src/components/Barchart/index.tsx
+++ frontend/src/components/Barchart/index.tsx
@@ -1,6 +1,36 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SuccessSale } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type seriesData = {
+    name: string,
+    data: number[]
+}
+
+type ChartData = {
+    labels: {categories: string[]};
+    series: seriesData[];
+}
+
 function Barchart (){
     
+    const [chartData, setChartData] = useState<ChartData>({labels: {categories: []}, series: [{name: "", data: []}]});
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sale-by-seller`)
+            .then(res => {
+                const data = res.data as SuccessSale[];
+
+                const myLabels = data.map( x => x.sellerName);
+                const mySeries = data.map( y => round(100*(y.deals / y.visited), 2));
+
+                setChartData({labels: {categories: myLabels}, series: [{name: "% Sucesso", data: mySeries}]})
+            })
+    }, [])
+
     const options = {
         plotOptions: {
             bar: {
@@ -8,7 +38,7 @@ function Barchart (){
             }
         },
     };
-    
+/* 
     const mockData = {
         labels: {
             categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -20,11 +50,11 @@ function Barchart (){
             }
         ]
     };
-    
+*/    
     return (
         <Chart 
-            options={{...options, xaxis: mockData.labels}}
-            series={mockData.series}
+            options={{...options, xaxis: chartData.labels}}
+            series={chartData.series}
             type="bar"
             height="240"
         />
--- frontend/src/components/Donutchart/index.tsx
+++ frontend/src/components/Donutchart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,23 +11,19 @@ type ChartData = {
 
 function Donutcharts () {
 
-    let chartData: ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({labels: [], series: []});
 
-    axios.get(`${BASE_URL}/sales/sum-by-seller`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sum-by-seller`)
         .then((res)=>{
             const data = res.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(y => y.sum);
 
-            chartData = { labels: myLabels, series: mySeries}
+            setChartData({ labels: myLabels, series: mySeries});
 
-            console.log(chartData);
         })
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
+    },[]);
     
     const options = {
         legend: {
